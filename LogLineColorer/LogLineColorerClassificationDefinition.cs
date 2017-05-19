9daafd5d78acd3b37ef2bbedf15e2a1af1870462
Nick Halvorsen <nhalvorsen@webstaurantstore.com>
2017-05-19 08:26:18 -0400

    initial implementation

---
+++ LogLineColorer/LogLineColorerClassificationDefinition.cs
@@ -0,0 +1,30 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="LogLineColorerClassificationDefinition.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace LogLineColorer
+{
+    /// <summary>
+    /// Classification type definition export for LogLineColorer
+    /// </summary>
+    internal static class LogLineColorerClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "LogLineColorer" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("LogLineColorer")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
---
+++ LogLineColorer/LogLineColorerFormat.cs
@@ -0,0 +1,34 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="LogLineColorerFormat.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace LogLineColorer
+{
+    /// <summary>
+    /// Defines an editor format for the LogLineColorer type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "LogLineColorer")]
+    [Name("LogLineColorer")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(After = Priority.High)] 
+    internal sealed class LogLineColorerFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogLineColorerFormat"/> class.
+        /// </summary>
+        public LogLineColorerFormat()
+        {
+            this.DisplayName = "logger.log lines"; // Human readable version of the name
+            this.ForegroundColor = Colors.DarkSlateGray;
+        }
+    }
+}
